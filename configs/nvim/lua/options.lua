f3a036c2a9c77cde20301e5a8ed3f0992ef1eb8a
Hojjat <safeith@gmail.com>
2025-11-14 23:00:54 +0100

    Enable treesitter-based folding in NvChad for improved code navigation

--- configs/nvim/lua/options.lua
+++ configs/nvim/lua/options.lua
@@ -3,3 +3,8 @@ require "nvchad.options"
 -- Custom options
 vim.opt.spelllang = "en_us"
 vim.opt.spell = true
+
+-- Treesitter folding
+vim.wo.foldmethod = "expr"
+vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.wo.foldenable = false  -- Disable folding by default (open all folds)
